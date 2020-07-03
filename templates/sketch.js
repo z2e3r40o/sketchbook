06bca4593a424e5fe3bc25d131bd88e613e844a2
z2e3r40o <z2e3r40o@protonmail.com>
2020-07-02 23:29:41 -0700

    manually manage pages in sketchbook

---
+++ templates/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+}
+
+function draw() {
+}
+
+//
+// do not remove what follows
+//
+
+var sketch = function(p5) {
+    p5.setup = setup;
+    p5.draw = draw;
+};
+
+new p5(sketch, window.document.getElementById('container'));
